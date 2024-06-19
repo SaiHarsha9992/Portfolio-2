95fd53ca2418864ebb89be80244c6ec4dc56ee5f
Kedarinadh Sai Harsha Gadu <22MH1A04G6@acoe.edu.in>
2024-06-19 13:39:11 +0530

    ha ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import App from "./App";
 import "./index.css";
 
