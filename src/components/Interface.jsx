5dd9ec30b5edbec7fa2a6ef60375b5aee597e82c
Gadu Kedarinadh Sai Harsha <119718764+SaiHarsha9992@users.noreply.github.com>
2024-08-19 10:10:43 +0530

    Update Interface.jsx

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -449,7 +449,7 @@ const ContactSection = () => {
           paddingRight:
             screenSize == "md" || screenSize == "sm" ? "50px" : "0px",
           paddingBottom:
-            screenSize == "md" || screenSize == "sm" ? "400px" : "400px",
+            screenSize == "md" || screenSize == "sm" ? "350px" : "400px",
         }}
       >
         <h2
